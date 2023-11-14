dc487a91f6a190c384137855104abe12205b598f
hasefumi23 <hasefumi23@gmail.com>
2023-11-14 22:39:37 +0900

    Complete 22.generate-parentheses.ts and 704.binary-search.ts

---
+++ 22.generate-parentheses.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=22 lang=typescript
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+function generateParenthesis(n: number): string[] {
+    
+};
+// @lc code=end
+
