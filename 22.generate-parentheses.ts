3ea274f3c58f2f52ebd57e940b0d98a32a69487c
hasefumi23 <hasefumi23@gmail.com>
2023-11-18 14:51:40 +0900

    Complete 22.generate-parentheses.ts

--- 22.generate-parentheses.ts
+++ 22.generate-parentheses.ts
@@ -6,7 +6,31 @@
 
 // @lc code=start
 function generateParenthesis(n: number): string[] {
-    
+  const stack: string[] = [];
+  const res: string[] = [];
+  
+  function backtrack(openN: number, closedN: number) {
+    if (openN === closedN && closedN === n && openN === n) {
+      res.push(stack.join(""));
+      return
+    }
+
+    if (openN < n) {
+      stack.push("(");
+      backtrack(openN + 1, closedN);
+      stack.pop();
+    }
+
+    if (closedN < openN) {
+      stack.push(")");
+      backtrack(openN, closedN + 1);
+      stack.pop();
+    }
+  }
+
+  backtrack(0, 0);
+  return res;
 };
 // @lc code=end
 
+console.log(generateParenthesis(3));
